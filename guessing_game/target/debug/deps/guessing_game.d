9c4c0659d7d4e9204ed66cdacb664df626dd1ccf
Ali Bagheri <a6bagher@uwaterloo.ca>
2023-07-13 11:10:34 -0400

    start of guessing game

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\16479\Desktop\rustLearning\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+C:\Users\16479\Desktop\rustLearning\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\16479\Desktop\rustLearning\guessing_game\target\debug\guessing_game.exe: C:\Users\16479\Desktop\rustLearning\guessing_game\src\main.rs
